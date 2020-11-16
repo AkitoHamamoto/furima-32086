dc6adb993e55635511b970a9b6dec38d827f0da8
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-11-16 17:49:28 +0900

    userモデル単体テストコード２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,14 @@ class User < ApplicationRecord
   has_many  :items
   has_many  :purchases
 
-  validates :nickname,        presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,           presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password,presence: true, length: { minimum: 6 }, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-  validates :first_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :kana_first_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :kana_last_name,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'には@のつくアドレスを入力してください' }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, presence: true, length: { minimum: 6 }, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,  message: 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須です' }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,  message: 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須です' }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須で' }
+  validates :kana_last_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須で' }
   validates :birth_date,      presence: true
-
-
 end
