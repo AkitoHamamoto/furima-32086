7441e765d638959ab20568ba9c198e98a927bd94
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-11-17 18:51:58 +0900

    販売手数料等の計算

---
+++ app/javascript/calced.js
@@ -0,0 +1,16 @@
+function calc () {
+  const input_price = document.getElementById("item-price");
+  const tax_price = document.getElementById("add-tax-price");
+  const profit_price = document.getElementById("profit");
+
+  input_price.addEventListener('input', () => {
+    const price = input_price.value
+    const tax = price * 0.1
+    const profit = price - tax
+    tax_price.innerHTML = tax
+    profit_price.innerHTML = profit
+  });
+ 
+}
+
+window.addEventListener("load", calc);
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
+  get 'items', to: 'items#calced'
   root to: "items#index"
   resources :items
 end
