e3e3543387b89c0730d51c70cb0abd53173e5e0f
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-11-17 12:22:39 +0900

    itemモデルのバリデーション2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
 
   with_options presense: true do
     validates :name
-    validates :price
+    validates :price,
+              numericality: {
+                only_integer: true, 
+                greater_than_or_equal_to: 300,
+                less_than_or_equal_to: 9999999 
+              }
+              PRICE_REGEX = /\A[0-9]+\z/
+              format: { with: PRICE_REGEX, message: '販売価格は半角数字のみ保存可能です'}
     validates :explanation
     validates :category_id 
     validates :condition_id
     validates :delivery_fee_id
     validates :prefecture_id
     validates :days_id
-    validates :user
   end
 
 end
