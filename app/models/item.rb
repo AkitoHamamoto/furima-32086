0f04b753993fdf4651887b5171123249705598f3
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-11-17 11:25:16 +0900

    itemモデルのバリデーション1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase
+
+  with_options presense: true do
+    validates :name
+    validates :price
+    validates :explanation
+    validates :category_id 
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :days_id
+    validates :user
+  end
+
 end
